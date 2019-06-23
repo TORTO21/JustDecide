db5f5bb37c98c7b25bd12f1255f41ba3d189b577
richierob62 <richierob62@gmail.com>
2019-06-23 02:26:31 -0400

    import reordering

--- server/server.js
+++ server/server.js
@@ -1,29 +1,30 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const db = require("../config/keys").MONGO_URI;
-const models = require('./models/index')
-const expressGraphQL = require("express-graphql");
-const schema = require('./schema/schema')
-const cors = require("cors");
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const expressGraphQL = require('express-graphql')
 
-const app = express();
+const db = require('../config/keys').MONGO_URI
+const models = require('./models/index')
+const schema = require('./schema/schema')
+
+const app = express()
 
 if (!db) {
-  throw new Error("You must provide a string to connect to MongoDB Atlas");
+  throw new Error('You must provide a string to connect to MongoDB Atlas')
 }
 
 mongoose
   .connect(db, { useNewUrlParser: true })
-  .then(() => console.log("Connected to MongoDB successfully"))
-  .catch(err => console.log(err));
+  .then(() => console.log('Connected to MongoDB successfully'))
+  .catch(err => console.log(err))
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
-app.use(cors());
+app.use(cors())
 
 app.use(
-  "/graphql",
+  '/graphql',
   expressGraphQL(req => {
     return {
       schema,
@@ -31,8 +32,8 @@ app.use(
         token: req.headers.authorization
       },
       graphiql: true
-    };
+    }
   })
-);
+)
 
-module.exports = app;
+module.exports = app
