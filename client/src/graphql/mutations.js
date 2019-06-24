c7645ef90b5b62f25f3015bb3a92ef48fdb7ae16
Oliver Almalel <oliver.almalel@gmail.com>
2019-06-24 13:42:58 -0700

    | fix id in service_auth | install apollo, graphQL, react libraries | create mutations and queries for frontend_auth |

---
+++ client/src/graphql/mutations.js
@@ -0,0 +1,32 @@
+import gql from 'graphql-tag';
+
+const Mutations = {
+  
+  REGISTER_USER: gql`
+    mutation RegisterUser($name: String!, $email: String!, $password: String!) {
+      register(name: $name, email: $email, password: $password) {
+        token
+        loggedIn
+      }
+    }
+  `,
+
+  LOGIN_USER: gql`
+    mutation LoginUser($phone_number: String!, $password: String!) {
+      login(phone_number: $phone_number, password: $password) {
+        token
+        loggedIn
+      }
+    }
+  `,
+  
+  VERIFY_USER: gql`
+    mutation VerifyUser($token: String!) {
+      verifyUser(token: $token) {
+        loggedIn
+      }
+    }
+  `,
+}
+
+export default Mutations
