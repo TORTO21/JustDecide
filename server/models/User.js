b5c08174f1555081379bea7b0477c6a638566c08
Oliver Almalel <oliver.almalel@gmail.com>
2019-06-24 09:31:49 -0700

    rebase master into user_auth

--- server/models/User.js
+++ server/models/User.js
@@ -3,7 +3,19 @@ const mongoose = require('mongoose')
 const { Schema } = mongoose
 
 const UserSchema = new Schema({
-  phone_hash: {
+    asks: {
+    type: Schema.Types.ObjectId,
+    ref: 'ask'
+  },
+  name: {
+    type: String,
+    required: false
+  },
+  email: {
+    type: String,
+    required: false
+  },
+  phone_number: {
     type: String,
     required: true
   },
@@ -42,8 +54,18 @@ const UserSchema = new Schema({
   monikers: {
     type: [Schema.Types.ObjectId],
     ref: 'moniker'
-  }
-})
+  },
+  password: {
+    type: String,
+    required: true,
+    min: 8,
+    max: 32
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
 const User = mongoose.model('user', UserSchema)
 module.exports = User
---
+++ server/validation/register.js
@@ -0,0 +1,37 @@
+const Validator = require("validator");
+const { validText, validNumber } = require("./manual_validations");
+
+module.exports = function validateRegisterInput(data) {
+  data.email = validText(data.email) ? data.email : "";
+  data.name = validText(data.name) ? data.name : "";
+  data.password = validText(data.password) ? data.password : "";
+  data.phone_number = validNumber(data.phone_number) ? data.phone_number : ""
+
+  if (data.email && !Validator.isEmail(data.email)) {
+    return { message: "Email is invalid", isValid: false }
+  }
+
+  // if (Validator.isEmpty(data.email)) {
+  //   return { message: "Email field is required", isValid: false }
+  // }
+
+  // if (Validator.isEmpty(data.name)) {
+  //   return { message: "Name field is required", isValid: false }
+  // }
+
+  console.log(data)
+
+  if (Validator.isEmpty(data.phone_number)) {
+    return { message: "Phone number is required", isValid: false }
+  }
+
+  if (!Validator.isNumeric(data.phone_number)) {
+    return { message: "Phone number must be numbers only", isValid: false }
+  }
+
+  if (!Validator.isLength(data.password, {min: 8, max: 32})) {
+    return { message: "Password length must be between 8 and 32 characters", isValid: false }
+  }
+
+  return { message: "", isValid: true }
+};
