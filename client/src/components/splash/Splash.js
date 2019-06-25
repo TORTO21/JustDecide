b7b6898c1223c778cededf6037a334197c1f7890
Lisa Steele <lisahsteele@gmail.com>
2019-06-24 18:27:36 -0700

    Pushing to remote so Master can have styling up until now

--- client/src/components/splash/Splash.js
+++ client/src/components/splash/Splash.js
@@ -3,28 +3,18 @@ import './Splash.css';
 
 const Splash = () => {
   return (
-    // <div className="background">
-    //   <div className="splash-title">just decide</div>
-    //   <div className="splash-subtitle">Ask a question and invite friends and family to weigh in</div>
-    //   <div className="splash-buttons">
-    //     <button className="create-ask-button">Create an Ask</button>
-    //     <div className="splash-auth-buttons">
-    //       <button className="solid-pink-button">Sign Up</button>
-    //       <button className="solid-pink-button">Login</button>
-    //     </div>
-    //   </div> 
-    // </div>
-    <div className="background parent">
-      <div className="rectangle turquoise"></div>
-      {/* <div className="form">
-        
-        <label className="form-label">Name</label>
-        <input className="input"></input>
-        testing
-        <input className="input"></input>
-      </div> */}
-      {/* <div className="gradient-pink-circle drop-shadow-circle"></div> */}
+    <div className="background">
+      <div className="splash-title">just decide</div>
+      <div className="splash-subtitle">Ask a question and invite friends and family to weigh in</div>
+      <div className="splash-buttons">
+        <button className="create-ask-button">Create an Ask</button>
+        <div className="splash-auth-buttons">
+          <button className="solid-pink-button">Sign Up</button>
+          <button className="solid-pink-button">Login</button>
+        </div>
+      </div> 
     </div>
+    
   )
 }
 
