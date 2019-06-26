3e81f75f6cfe88da4573a9a40b4775c3b9497bb3
Lisa Steele <lisahsteele@gmail.com>
2019-06-26 13:41:21 -0700

    Arrows on time picker hooked up to correctly increment/decrement time

--- client/src/components/calendar/TimePicker.js
+++ client/src/components/calendar/TimePicker.js
@@ -4,6 +4,7 @@ import dateFns from 'date-fns';
 import './TimePicker.css';
 import TimePickerUp from '../icons/TimePickerUp.png';
 import TimePickerDown from '../icons/TimePickerDown.png';
+import { throwServerError } from 'apollo-link-http-common';
 
 class TimePicker extends React.Component {
   constructor(props) {
@@ -15,22 +16,42 @@ class TimePicker extends React.Component {
       meridiemIndicator: "PM"
     }
     this.meridiemToggle = this.meridiemToggle.bind(this)
+    this.prevHour = this.prevHour.bind(this)
+    this.nextHour = this.nextHour.bind(this)
+    this.prevMin = this.prevMin.bind(this)
+    this.nextMin = this.nextMin.bind(this)
   }
 
   prevHour() {
-
+    let prevHour = dateFns.subHours(this.state.now, 1)
+    this.setState({
+      now: prevHour,
+      hr: prevHour
+    })
   }
 
   nextHour() {
-
+    let nextHour = dateFns.addHours(this.state.now, 1)
+    this.setState({
+      now: nextHour,
+      hr: nextHour
+    })
   }
 
   prevMin() {
-
+    let prevMin = dateFns.subMinutes(this.state.now, 1)
+    this.setState({
+      now: prevMin,
+      min: prevMin
+    })
   }
 
   nextMin() {
-
+    let nextMin = dateFns.addMinutes(this.state.now, 1)
+    this.setState({
+      now: nextMin,
+      min: nextMin
+    })
   }
 
   meridiemToggle() {
@@ -43,8 +64,8 @@ class TimePicker extends React.Component {
   }
 
   render() {
-    const hrFormat = "HH"
-    const minFormat = "MM"
+    const hrFormat = "hh"
+    const minFormat = "mm"
     const meridiemFormat = "A"
     if (dateFns.format(new Date(), meridiemFormat) === "AM") {
       this.setState({ meridiemIndicator: "AM"})
@@ -54,14 +75,26 @@ class TimePicker extends React.Component {
         <div className="time-picker">
           <div className="input-group">
             <div className="time-input">
-              <img src={TimePickerUp} className="time-arrows"></img>
+              <img 
+                src={TimePickerUp} 
+                className="time-arrows"
+                onClick={this.nextHour}></img>
               {dateFns.format(this.state.hr, hrFormat)}
-              <img src={TimePickerDown} className="time-arrows"></img>
+              <img 
+                src={TimePickerDown} 
+                className="time-arrows"
+                onClick={this.prevHour}></img>
             </div>
             <div className="time-input">
-              <img src={TimePickerUp} className="time-arrows"></img>
-              {dateFns.format(this.state.min, minFormat)}
-              <img src={TimePickerDown} className="time-arrows"></img>
+              <img 
+                src={TimePickerUp} 
+                className="time-arrows"
+                onClick={this.nextMin}></img>
+              {dateFns.format(this.state.now, minFormat)}
+              <img 
+                src={TimePickerDown} 
+                className="time-arrows"
+                onClick={this.prevMin}></img>
             </div>
             <div className="time-input">
               <img 
