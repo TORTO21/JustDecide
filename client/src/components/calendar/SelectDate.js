5bf4ec15635cf37b010015b5c15f9b8abc5efd6f
Lisa Steele <lisahsteele@gmail.com>
2019-06-25 23:24:18 -0700

    Calendar is styled to look like the design but missing some functionality. (select icon, and selecting a month/year)

--- client/src/components/calendar/SelectDate.js
+++ client/src/components/calendar/SelectDate.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import gql from 'graphql-tag';
 import './Calendar.css';
-import dateFns, { getISOWeek, isThisISOWeek } from 'date-fns';
-import { Mutation, ApolloProvider, ApolloConsumer } from 'react-apollo';
+import dateFns from 'date-fns';
+import { ApolloConsumer } from 'react-apollo';
+import RightArrow from '../icons/RightArrow.png';
+import LeftArrow from '../icons/LeftArrow.png';
+import PinkCheck from '../icons/PinkCheck.png';
 
 class SelectDate extends React.Component {
   constructor(props) {
@@ -13,13 +15,15 @@ class SelectDate extends React.Component {
       today: new Date(),
       currentWeekStart: dateFns.startOfWeek(new Date()),
       currentWeekEnd: dateFns.endOfWeek(new Date()),
-      monthSelector: false
+      monthSelector: false, 
+      selectState: false
     }
     this.handleNext = this.handleNext.bind(this);
     this.updateCache = this.updateCache.bind(this);
     this.nextWeek = this.nextWeek.bind(this);
     this.prevWeek = this.prevWeek.bind(this);
     this.handleMonthClick = this.handleMonthClick.bind(this);
+    this.handleClearMonths = this.handleClearMonths.bind(this);
   }
 
   componentDidUpdate(prevProps, prevState) {
@@ -34,22 +38,23 @@ class SelectDate extends React.Component {
     return (
       <div className="header row flex-middle">
         <div className="col col-start">
-          <div className="icon" onClick={this.prevWeek}>
-            chevron_left
-          </div>
+          <img
+            src={LeftArrow}
+            className="arrow"
+            onClick={this.prevWeek}>
+          </img>
         </div>
-        <div className="col col-center">
+        <div className="col col-center month-year">
           <div onClick={this.handleMonthClick}>
             {dateFns.format(this.state.currentMonth, dateFormat)}
           </div>
-          {/* <span>
-            {dateFns.format(this.state.currentMonth, dateFormat)}
-          </span> */}
         </div>
         <div className="col col-end">
-          <div className="icon" onClick={this.nextWeek}>
-            chevron_right
-          </div>
+          <img 
+            src={RightArrow}
+            className="arrow"
+            onClick={this.nextWeek}>
+          </img>
         </div>
       </div>
     )
@@ -74,7 +79,7 @@ class SelectDate extends React.Component {
   renderDates() {
     const { currentMonth, selectedDate } = this.state;
     const monthStart = dateFns.startOfMonth(currentMonth);
-    const today = new Date() 
+    // const today = new Date() 
 
     const currentWeekStart = this.state.currentWeekStart
     const currentWeekEnd = this.state.currentWeekEnd
@@ -87,21 +92,34 @@ class SelectDate extends React.Component {
     let day = currentWeekStart;
     let formattedDate = "";
 
+    let dateCell;
+
+    if (this.state.selectState === "false") {
+      dateCell = (
+        <>
+          <span className="number">{formattedDate}</span>
+          <span className="bg">{formattedDate}</span>
+        </>
+      )
+    } else {
+      dateCell = (
+        <img src={PinkCheck} className="calendar-pink-check"></img>
+      )
+    }
+
     while (day <= currentWeekEnd) {
       for (let i = 0; i < 7; i++) {
         formattedDate = dateFns.format(day, dateFormat);
         const cloneDay = day;
         days.push(
           <div
-            className={`col cell ${
-              !dateFns.isSameMonth(day, monthStart)
-              ? "disabeled"
-              : dateFns.isSameDay(day, selectedDate) ? "selected" : ""
-            }`}
+            className={`col cell ${!dateFns.isSameMonth(day, monthStart) ? "disabeled" : dateFns.isSameDay(day, selectedDate) ? "selected" : ""}`}
+            // className="col cell"
+            // className={this.state.selectState === false ? "col cell" : "calendar-pink-check"}
             key={day}
             onClick={() => this.onDateClick(dateFns.parse(cloneDay), currentWeekStart)}>
             <span className="number">{formattedDate}</span>
-            <span className="bg">{formattedDate}</span>
+            {/* {dateCell} */}
           </div>
         );
         day = dateFns.addDays(day, 1);
@@ -116,22 +134,6 @@ class SelectDate extends React.Component {
     return <div className="body">{rows}</div>
   }
 
-  renderMonthSelector() {
-    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-    if (this.state.monthSelector === true) {
-      months.forEach(month => {
-        return (
-          <div className="month-selector">
-            Testing
-            <ul>
-              {month}
-            </ul>
-          </div>
-        )
-      })
-    }
-  }
-
   nextWeek() {
     let nextWeek = dateFns.addWeeks(this.state.today, 1)
     let nextWeekStart = dateFns.startOfWeek(nextWeek)
@@ -139,7 +141,8 @@ class SelectDate extends React.Component {
     this.setState({
       today: nextWeek,
       currentWeekStart: nextWeekStart,
-      currentWeekEnd: nextWeekEnd
+      currentWeekEnd: nextWeekEnd,
+      currentMonth: nextWeek
     })
     return nextWeek
   }
@@ -151,7 +154,8 @@ class SelectDate extends React.Component {
     this.setState({
       today: prevWeek,
       currentWeekStart: prevWeekStart,
-      currentWeekEnd: prevWeekEnd 
+      currentWeekEnd: prevWeekEnd,
+      currentMonth: prevWeek
     })
     return prevWeek
   }
@@ -164,7 +168,8 @@ class SelectDate extends React.Component {
 
   onDateClick = (day) => {
     this.setState({
-      selectedDate: day
+      selectedDate: day,
+      selectState: true
     });
   };
 
@@ -203,22 +208,49 @@ class SelectDate extends React.Component {
     // console.log(this.state.selectedDate.getFullYear())
   }
 
+  handleClearMonths(e) {
+    // e.stopPropagation();
+    this.setState({
+      monthSelector: false
+    })
+  }
+
   render() {
-    
+    let months;
+    const monthOptions = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+    if (this.state.monthSelector === true) {
+      months = monthOptions.map((month, i) => {
+        return (
+          <li key={i} className="month">
+            {month}
+          </li>
+        )
+      })
+    } else {
+      months = ""
+    }
+
     return (
       <ApolloConsumer>
         {(client) => {
           return(
-            <div className="calendar">
-              {this.renderHeader()}
-              {this.renderMonthSelector()}
-              {this.renderDays()}
-              {this.renderDates()}
-              <button 
-                className="solid-pink-button"
-                onClick={() => this.updateCache(client)}>
-                Next
-              </button>
+            <div className="background">
+              <div className="section-header cal-header">Set the Date</div>
+              <div className="calendar">
+                {this.renderHeader()}
+                {/* <ul className="month-selector">
+                  {months}
+                </ul> */}
+                {this.renderDays()}
+                {this.renderDates()}
+              </div>
+              <div className="cal-button-container">
+                <button 
+                  className="solid-pink-button calendar-button"
+                  onClick={() => this.updateCache(client)}>
+                  Next
+                </button>
+              </div>
             </div>
           )
         }} 
