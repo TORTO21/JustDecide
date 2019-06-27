a80e2d07c5e037ea5a68509088188850a888dddb
Lisa Steele <lisahsteele@gmail.com>
2019-06-26 22:08:38 -0700

    Input option now being added and rendered to the list view below

--- client/src/components/asks/AskOption.js
+++ client/src/components/asks/AskOption.js
@@ -1,27 +1,70 @@
 import React from 'react';
 import { ApolloConsumer } from 'react-apollo';
 import './AskOption.css';
+import Switch from 'react-switch';
 
 class AskOption extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      checked: false,
+      options: [], 
+      newOption: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.updateList = this.updateList.bind(this)
+    this.handleInput = this.handleInput.bind(this)
   }
 
+  handleChange(checked) {
+    this.setState({ checked })
+  }
+
+  handleInput() {
+    return (e) => {
+      this.setState({ newOption: e.target.value})
+    }
+  }
+
+  updateList() {
+    console.log(this.state.newOption)
+    let options = this.state.options
+    if (!options.includes(this.state.newOption)) {
+      options.push(this.state.newOption)
+    }
+    this.setState({ options: options }, console.log(this.state.options))
+  }
+  
   render() {
+    let optionList = this.state.options.map((option, i) => {
+      return (
+        <li key={i}>
+          {option}
+        </li>
+      )
+    })
     return(
       <div className="background">
         <div className="">
           <div className="top-container">
             <div className="section-header ask-option-header">Is this a yes or no question?</div>
-            <div>toggle</div>
+            <label>
+              <Switch 
+                onChange={this.handleChange} 
+                checked={this.state.checked} 
+                className="toggle" />
+            </label>
           </div>
           <div className="lower-container">
             <input
               className="option-input drop-shadow"
-              placeholder="option">
+              placeholder="option"
+              type="text"
+              onChange={this.handleInput()}>
             </input>
             <button
-              className="add-button green-gradient">
+              className="add-button green-gradient"
+              onClick={this.updateList}>
               Add Option
             </button>
             <button
@@ -29,7 +72,7 @@ class AskOption extends React.Component {
               Continue
             </button>
             <ul className="option-list-view drop-shadow">
-              List view
+              {optionList}
             </ul>
           </div>
         </div>
