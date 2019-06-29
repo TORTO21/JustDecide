443aebcb47b144f1ea1120342c0b97a4d6c7fc74
richierob62 <richierob62@gmail.com>
2019-06-29 09:45:57 -0400

    Fix errors generated by earlier merge conflict fix

--- client/src/components/asks/AskSuccess.js
+++ client/src/components/asks/AskSuccess.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import { ApolloConsumer, Query } from 'react-apollo';
-import './AskSuccess.css';
-import { FETCH_ASK_DETAILS } from '../../graphql/queries/user_queries';
-import PinkCheck from '../../components/icons/PinkCheck.png';
+import './AskSuccess.css'
+
+import { ApolloConsumer, Query } from 'react-apollo'
+
+import Countdown from './Countdown'
+import { FETCH_ASK_DETAILS } from '../../graphql/queries/user_queries'
+import PinkCheck from '../../components/icons/PinkCheck.png'
+import React from 'react'
 import dateFns from 'date-fns'
-import Countdown from './Countdown';
 
 class AskSuccess extends React.Component {
   constructor(props) {
@@ -12,7 +14,7 @@ class AskSuccess extends React.Component {
   }
 
   render() {
-    return( 
+    return (
       <ApolloConsumer>
         {client => (
           <Query query={FETCH_ASK_DETAILS}>
@@ -22,26 +24,28 @@ class AskSuccess extends React.Component {
                 return (
                   <li key={i} className="invitee-li">
                     {option}
-                    <img src={PinkCheck} className="success-pink-check"></img>
+                    <img src={PinkCheck} className="success-pink-check" />
                   </li>
                 )
               })
 
-              let arr = data.askDate.split(" ")
-              let time = arr[3] + " " + arr[4]
+              let arr = data.askDate.split(' ')
+              let time = arr[3] + ' ' + arr[4]
               let d = new Date(data.askDate)
-              let dateFormat = "MMMM D, YYYY"
+              let dateFormat = 'MMMM D, YYYY'
               const date = dateFns.format(d, dateFormat)
 
               let milliseconds = new Date(data.deadlineDate).getTime()
-              
-              return(
+
+              return (
                 <div className="background">
                   <div className="overall-container">
                     <div className="top-container">
                       <div className="section-header">Success!</div>
                       <div className="ask-details-container">
-                        <div className="ask-details">Where do you want to go to dinner?</div>
+                        <div className="ask-details">
+                          Where do you want to go to dinner?
+                        </div>
                         <div className="date-time-group">
                           <div className="ask-date">{date}</div>
                           <div className="ask-time">{time}</div>
@@ -53,16 +57,16 @@ class AskSuccess extends React.Component {
                         <Countdown deadline={milliseconds} />
                       </div>
                       <div className="ask-invitees-list">
-                        <ul className="ask-invitees-ul">
-                          {options}
-                        </ul>
+                        <ul className="ask-invitees-ul">{options}</ul>
                       </div>
-                      <button className="ask-button solid-pink-button">Ask!</button>
+                      <button className="ask-button solid-pink-button">
+                        Ask!
+                      </button>
                     </div>
                   </div>
                 </div>
               )
-            }} 
+            }}
           </Query>
         )}
       </ApolloConsumer>
@@ -70,4 +74,4 @@ class AskSuccess extends React.Component {
   }
 }
 
-export default AskSuccess;
+export default AskSuccess
