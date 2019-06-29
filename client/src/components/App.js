a0f37bd818097757423ecce6730a920e2c4f4294
richierob62 <richierob62@gmail.com>
2019-06-29 09:38:10 -0400

    Fix duplicate imports generated from pull request

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,5 @@
 import '../index.css'
+import '../index.css'
 
 import { Route, Switch } from 'react-router-dom'
 
@@ -7,6 +8,7 @@ import AskDetail from './asks/AskDetail'
 import AskInvite from './asks/AskInvite'
 import AskOption from './asks/AskOption'
 import AskQuestion from './ask_question/AskQuestion'
+import AskSuccess from './asks/AskSuccess'
 import AuthRoute from '../util/route_util'
 import Deadline from './calendar/Deadline'
 import Login from './auth/Login'
@@ -16,13 +18,6 @@ import React from 'react'
 import Register from './auth/Register'
 import SelectDate from './calendar/SelectDate'
 import Splash from './splash/Splash'
-import AskQuestion from './asks/AskQuestion'
-import SelectDate from './calendar/SelectDate';
-import Deadline from './calendar/Deadline';
-import AskOption from './asks/AskOption';
-import AskSuccess from './asks/AskSuccess';
-import '../index.css';
-
 
 const App = () => (
   <div>
@@ -33,11 +28,11 @@ const App = () => (
       <Route path="/asks/new" component={AskQuestion} />
       <Route exact path="/asks/:ask_id" component={AskDetail} />
       <Route exact path="/asks/:ask_id/new-option" component={NewOption} />
-      <Route exact path="/selectDate" component={ SelectDate } />
-      <Route exact path="/deadlineDate" component={ Deadline } />
-      <Route exact path="/askOption" component={ AskOption } />
-      <Route exact path="/askSuccess" component={ AskSuccess } />
-      <Route path="/" component= { Splash } />
+      <Route exact path="/selectDate" component={SelectDate} />
+      <Route exact path="/deadlineDate" component={Deadline} />
+      <Route exact path="/askOption" component={AskOption} />
+      <Route exact path="/askSuccess" component={AskSuccess} />
+      <Route path="/" component={Splash} />
       <Route exact path="/askInvite" component={AskInvite} />
       <Route exact path="/users/:user_id/newContact" component={AddContact} />
       {/* <Route path="/asks" component= { AsksIndex } /> */}
