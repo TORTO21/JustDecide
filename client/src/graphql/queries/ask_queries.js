5e02084d4312f8c3a0cb0c1c8f44597c49f0da6d
Lisa Steele <lisahsteele@gmail.com>
2019-07-25 11:07:25 -0700

    Wrapped GET_ASK query. Tested with a hardcoded ask id and it works

--- client/src/graphql/queries/ask_queries.js
+++ client/src/graphql/queries/ask_queries.js
@@ -21,56 +21,56 @@ export const FETCH_ASK_DETAILS = gql`
   }
 `
 
-export const GET_ASK = gql`
-  query getAsk($id: ID!) {
-    ask(id: $id) {
-      id
-      author {
-        id
-      }
-      name_used {
-        name
-      }
-      question
-      use_date
-      use_time
-      date
-      deadline
-      invitations {
-        id
-        contact {
-          phone_number
-          id
-          user {
-            id
-          }
-          name
-        }
-      }
-      options {
-        id
-        creator {
-          user {
-            id
-          }
-          name
-        }
-        title
-        votes {
-          id
-          contact {
-            phone_number
-            user {
-              id
-            }
-            name
-          }
-          direction
-        }
-      }
-    }
-  }
-`
+// export const GET_ASK = gql`
+//   query getAsk($id: ID!) {
+//     ask(id: $id) {
+//       id
+//       author {
+//         id
+//       }
+//       name_used {
+//         name
+//       }
+//       question
+//       use_date
+//       use_time
+//       date
+//       deadline
+//       invitations {
+//         id
+//         contact {
+//           phone_number
+//           id
+//           user {
+//             id
+//           }
+//           name
+//         }
+//       }
+//       options {
+//         id
+//         creator {
+//           user {
+//             id
+//           }
+//           name
+//         }
+//         title
+//         votes {
+//           id
+//           contact {
+//             phone_number
+//             user {
+//               id
+//             }
+//             name
+//           }
+//           direction
+//         }
+//       }
+//     }
+//   }
+// `
 
 export const GET_ASKS = gql`
   {
